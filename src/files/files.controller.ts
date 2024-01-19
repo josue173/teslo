42f0d8e861e2518c2e332db3459b8c2cd9083dac
josue173 <josue.perez04@outlook.com>
2024-01-18 18:57:39 -0600

    Guardar imagenes en filesystem

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,18 +8,30 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/filefilter';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 100 },
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
   // Nombre de la propiedad del body que se espera y el intercepto r
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
-      throw new BadRequestException('Make sure that you are uploading a picture')
-    } 
+      throw new BadRequestException(
+        'Make sure that you are uploading a picture',
+      );
+    }
+
     return { fileName: file.originalname };
   }
 }
