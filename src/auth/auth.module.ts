8e196588744ef73b00c51105d991cd2017eceac9
josue173 <josue.perez04@outlook.com>
2024-01-24 20:25:15 -0600

    Fin de validaciones JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
@@ -32,6 +34,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     //   },
     // }),
   ],
-  exports: [TypeOrmModule], // Esto exporta la configuración de la línea superior
+  exports: [
+    TypeOrmModule,
+    JwtStrategy,
+    TypeOrmModule,
+    PassportModule,
+    JwtModule,
+  ], // Esto exporta la configuración de la línea superior
 })
 export class AuthModule {}
