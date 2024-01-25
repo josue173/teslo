cc88ab1bc8855d3527a9f544d2f6042342e7a3f5
josue173 <josue.perez04@outlook.com>
2024-01-25 17:45:10 -0600

    Agregando Guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto, CreateUserDto } from './dto/indes';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,11 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard())
+  testingPrivateRoute() {
+    return 'hi';
   }
 }
