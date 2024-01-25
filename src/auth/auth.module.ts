a9c5b8fe44c4e5159535be52d1c3af98fae8a676
josue173 <josue.perez04@outlook.com>
2024-01-24 18:20:34 -0600

    Inicua configuracion de JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h',
+    //   },
+    // }),
+  ],
   exports: [TypeOrmModule], // Esto exporta la configuración de la línea superior
 })
 export class AuthModule {}
